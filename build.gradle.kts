7c6f815462a19c2afa3f5ab68ed91025912bffe8
HatDog20 <kecaupit@gmail.com>
2024-03-31 23:09:06 +0800

    FINAL

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+    implementation("io.github.microutils:kotlin-logging:1.12.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
